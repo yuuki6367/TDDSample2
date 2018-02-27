e8d741f91f3a904b807e7b0fcb627b733b81eff8
Tomo Masakura <tomo.masakura@gmail.com>
2018-02-27 16:08:37 +0900

    Initial commit

---
+++ TDDSample.Web/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TDDSample.Web
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal sealed class Startup
+    {
+        // ReSharper disable once UnusedMember.Global
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
