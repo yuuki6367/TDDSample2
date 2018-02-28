e7513f1a5186cdd31dee40828843588ad71b6057
Tomo Masakura <tomo.masakura@gmail.com>
2018-02-28 15:50:42 +0900

    しっぱいするてすとこーどをついか

---
+++ TDDSample.Web.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TDDSample.Web.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+    }
+}
