b45c58d00a70ca0520bb9c26ca7121b65c70c362
y.kawabata <yuuki6367@gmail.com>
2018-03-03 15:41:39 +0900

    一通り関数が出来た気がするー

---
+++ TDDSample.Web.Tests/RentalTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal.Networking;
+using SQLitePCL;
+using TDDSample.Web.Models.Rentals;
+using Xunit;
+
+namespace TDDSample.Web.Tests
+{
+    public class RentalTest
+    {
+        private readonly Movie _movie;
+        private readonly Customer _customer;
+
+        public RentalTest()
+        {
+            _movie = new Movie("アウトレイジ ビヨンド", MovieRentalType.NewRelease);
+            _customer = new Customer("太郎");
+        }
+
+        [Fact]
+        public void 新作を一泊借りたら300円()
+        {
+            _customer.AddRental(new Rental(_movie, 1));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("300円", result);
+        }
+
+        [Fact]
+        public void 新作を三泊借りたら900円()
+        {
+            _customer.AddRental(new Rental(_movie, 3));
+
+            var result = _customer.RentalFee();
+
+            Assert.Equal(900, result);
+        }
+
+        [Fact]
+        public void 整形1200円にはカンマが入る()
+        {
+            _customer.AddRental(new Rental(_movie, 4));
+
+            var result = _customer.Statement();
+
+            Assert.Equal("1,200円", result);
+        }
+
+        [Fact]
+        public void 普通を二泊借りたら200円()
+        {
+            _customer.AddRental(new Rental(new Movie("釣りバカ日誌", MovieRentalType.Regular), 2));
+
+            var result = _customer.RentalFee();
+
+            Assert.Equal(200, result);
+        }
+
+        [Fact]
+        public void 普通を三泊借りたら400円()
+        {
+            _customer.AddRental(new Rental(new Movie("釣りバカ日誌", MovieRentalType.Regular), 3));
+
+            var result = _customer.RentalFee();
+
+            Assert.Equal(400, result);
+        }
+
+        [Fact]
+        public void 新作を一泊と普通を二泊借りたら500円()
+        {
+            _customer.AddRental(new Rental(new Movie("シンゴジラ", MovieRentalType.NewRelease), 1));
+            _customer.AddRental(new Rental(new Movie("釣りバカ日誌", MovieRentalType.Regular), 2));
+
+            var result = _customer.RentalFee();
+
+            Assert.Equal(500, result);
+        }
+        
+        [Fact]
+        public void 子供が三泊借りたら150円()
+        {
+            _customer.AddRental(new Rental(new Movie("釣りバカ日誌", MovieRentalType.Children), 3));
+
+            var result = _customer.RentalFee();
+
+            Assert.Equal(150, result);
+        }
+    }
+}
--- TDDSample.Web/Models/Rentals/Customer.cs
+++ TDDSample.Web/Models/Rentals/Customer.cs
@@ -22,7 +22,48 @@ namespace TDDSample.Web.Models.Rentals
          */
         public string Statement()
         {
-            return null;
+            var fee = RentalFee();
+            return $"{fee:N0}円";
+        }
+
+        public int RentalFee()
+        {
+            var result = 0;
+            foreach (var rental in _rentals)
+            {
+                result += RentalFee(rental);
+            }
+            return result;
+        }
+
+        private static int RentalFee(Rental rental)
+        {
+            var result = 0;
+
+            switch (rental.Movie.RentalType)
+            {
+                case MovieRentalType.NewRelease:
+                    result += rental.DaysRented * 300;
+                    break;
+                case MovieRentalType.Children:
+                    result += 150;
+                    for (var i = 3; i < rental.DaysRented; i++)
+                    {
+                        result += 150;
+                    }
+
+                    break;
+                default:
+                    result += 200;
+                    for (var i = 2; i < rental.DaysRented; i++)
+                    {
+                        result += 200;
+                    }
+
+                    break;
+            }
+
+            return result;
         }
     }
 }
